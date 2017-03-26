aa4d9ffb8d0dd694473d304b183db4bdee29f567
Prototype <263693992@qq.com>
2017-03-26 22:20:07 +0800

    pyte

--- demo.py
+++ demo.py
@@ -5,13 +5,17 @@ from PyQt4.QtGui import QApplication
 
 from pyqterm import TerminalWidget
 
+shell = {
+    'python2.7': '/usr/bin/python2.7',
+    'bash': '/bin/bash'
+}
 
 if __name__ == "__main__":
     import signal
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     app = QApplication(sys.argv)
-    win = TerminalWidget(command='/usr/bin/python2.7')
+    win = TerminalWidget(command=shell['bash'], font_size=16)
     win.show()
     app.exec_()
 
